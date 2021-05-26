f2e2a95a834fbe17391fe920f6db9bb111582c16
Camilo <jcamilovelandiab@gmail.com>
2021-05-25 21:19:28 -0500

    Adding flex layout library to the project and using FlexLayoutModule in the heroes module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AddComponent } from './pages/add/add.component';
 import { HeroComponent } from './pages/hero/hero.component';
 import { HeroListComponent } from './pages/hero-list/hero-list.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
 
 
 @NgModule({
@@ -19,6 +21,7 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule
   ]
 })
